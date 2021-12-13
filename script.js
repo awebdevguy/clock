322a57841ab9de80e37d88d29abba89cd50308dd
awebdevguy <awebdevguy@gmail.com>
2021-12-13 07:20:06 -0700

    change script.js

--- script.js
+++ script.js
@@ -2,20 +2,18 @@ const time = document.getElementById('time');
 const ampm = document.getElementById('ampm');
 
 window.onload = function () {
-  DisplayCurrentTime();
+  DisplayTime();
 };
 
-function DisplayCurrentTime() {
+function DisplayTime() {
   let date = new Date();
   let hours = date.getHours() > 12 ? date.getHours() - 12 : date.getHours();
-  let am_pm = date.getHours() >= 12 ? "PM" : "AM";
+  let amPM = date.getHours() >= 12 ? "PM" : "AM";
   hours = hours < 10 ? "0" + hours : hours;
   let minutes = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
-  // var seconds = date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
-  // systemTime = hours + ":" + minutes + ":" + seconds + " " + am_pm;
   systemTime = hours + ":" + minutes;
   time.innerHTML = systemTime;
-  ampm.innerHTML = am_pm;
+  ampm.innerHTML = amPM;
 };
 
 setInterval(() => {
