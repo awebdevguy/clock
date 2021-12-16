9755fa763fb5786eab9eb4c76258f4d2699759e9
awebdevguy <awebdevguy@gmail.com>
2021-12-16 08:36:36 -0700

    add light mode on click

--- script.js
+++ script.js
@@ -5,11 +5,17 @@ window.onload = function () {
   displayTime();
 };
 
+window.document.addEventListener('click', () => {
+  const html = document.querySelector('html');
+
+  html.classList.contains('light') ? html.classList.remove('light') : html.classList.add('light');
+});
+
 function displayTime() {
   let date = new Date();
   let hours = date.getHours() > 12 ? date.getHours() - 12 : date.getHours();
   let amPM = date.getHours() >= 12 ? "PM" : "AM";
-  hours = hours < 10 ? "0" + hours : hours;
+  // hours = hours < 10 ? "0" + hours : hours;
   let minutes = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
   // let seconds = date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
   // systemTime = hours + ":" + minutes + ":" + seconds;
